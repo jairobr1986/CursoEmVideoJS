ff10337fbe7e34bb2f8d1b54433cbebf458a6377
Jairo Oliveira Santos <jairobr1986@gmail.com>
2025-10-15 07:43:02 -0300

    Terminando exercicio

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -3,16 +3,30 @@ function contar() {
     let inicio = document.getElementById("txti");
     let fim = document.getElementById("txtf");
     let passo = document.getElementById("txtp");
+    let res = document.getElementById("res")
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert("ERROR - faltam dados para ser inseridos.");
     } else {
-        res.innerHTML = "Contando..."
+        res.innerHTML = "Contando... <br>"
         let i = Number(inicio.value);
         let f = Number(fim.value);
         let p = Number(passo.value);
-        for (let contador = i; contador <= f; contador += p) {
-            res.innerHTML += `${contador} \u{1F449}`
+        if (p <= 0) {
+            window.alert("Passo Invalido! Considerando PASSO 1");
+            p = 1;
         }
+        if (i < f) {
+            for (let contador = i; contador <= f; contador += p) {
+                res.innerHTML += `${contador} \u{1F449}`;
+            }
+
+        } else {
+            for (let contador = i; contador >= f; contador -= p) {
+                res.innerHTML += `${contador} \u{1F449}`;
+
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`;
     }
 }
 
